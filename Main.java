93408ee3da64d627748c6070cc76fa6460a66f10
SudhanshuRoy <shudhanshukumarmuz@gmail.com>
2024-01-16 23:15:13 +0530

    this is suyog

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
-public class Main{
+public class Main {
 
-public static void main(String [] args){
-System.out.println("I am output from jenkins-test project");
-System.out.println("This is sudhanshu roy !");
-}
+    public static void main(String[] args) {
+        System.out.println("I am output from jenkins-test project");
+        System.out.println("This is sudhanshu roy !");
+        System.out.println("this is suyog");
+    }
 
 }
